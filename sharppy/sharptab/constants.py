d994b60eb65a31c6f226efdd7aa3d54c8609299a
Patrick Marsh <patrickmarshwx@gmail.com>
2011-12-24 12:50:14 -0600

    Added gravity as a constant

--- sharppy/sharptab/constants.py
+++ sharppy/sharptab/constants.py
@@ -1,7 +1,8 @@
 ''' Frequently used constants '''
 
-__all__ = ["RMISSD", "ROCP", "ZEROCNK"]
+__all__ = ["RMISSD", "ROCP", "ZEROCNK", "G"]
 
 RMISSD = -9999.0            # Missing Flag
 ROCP = 0.28571426           # R divided by Cp
 ZEROCNK = 273.15            # Zero Celsius in Kelvins
+G = 9.80665                 # Earth's Gravity
