85011efec564d46009f3bcbe082d02249cbf4fc9
Patrick Marsh <patrickmarshwx@gmail.com>
2011-12-26 09:24:03 -0600

    Changed QC routine to new function call

--- sharppy/sharptab/winds.py
+++ sharppy/sharptab/winds.py
@@ -20,7 +20,7 @@ def vec2comp(dir, spd):
         u       (float)         U-component
         v       (float)         V-component
     '''
-    if not qc(dir) or not qc(spd): return RMISSD
+    if not QC(dir) or not QC(spd): return RMISSD
     u = spd * math.sin(math.radians(dir)) * -1
     v = spd * math.cos(math.radians(dir)) * -1
     return u, v
@@ -40,7 +40,7 @@ def comp2vec(u, v):
         dir    (float)         direction (degrees)
         spd    (float)         speed
     '''
-    if not qc(u) or not qc(v): return RMISSD
+    if not QC(u) or not QC(v): return RMISSD
     dir =  math.degrees(math.atan2(-u, -v))
     spd = mag(u, v)
     return dir, spd
