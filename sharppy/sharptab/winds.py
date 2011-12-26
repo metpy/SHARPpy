74c97fb363c359cfb95542c2b624a6cce5574ff6
Patrick Marsh <patrickmarshwx@gmail.com>
2011-12-26 09:58:39 -0600

    Fixed bug where wind routines that return 2 variables were only returning 1 if data was not verified. Also added QC checks to mag function

--- sharppy/sharptab/winds.py
+++ sharppy/sharptab/winds.py
@@ -20,7 +20,7 @@ def vec2comp(dir, spd):
         u       (float)         U-component
         v       (float)         V-component
     '''
-    if not QC(dir) or not QC(spd): return RMISSD
+    if not QC(dir) or not QC(spd): return RMISSD, RMISSD
     u = spd * math.sin(math.radians(dir)) * -1
     v = spd * math.cos(math.radians(dir)) * -1
     return u, v
@@ -40,7 +40,7 @@ def comp2vec(u, v):
         dir    (float)         direction (degrees)
         spd    (float)         speed
     '''
-    if not QC(u) or not QC(v): return RMISSD
+    if not QC(u) or not QC(v): return RMISSD, RMISSD
     dir =  math.degrees(math.atan2(-u, -v))
     spd = mag(u, v)
     return dir, spd
@@ -59,4 +59,5 @@ def mag(u, v):
     -------
         Returns the magnitude of a vector (float)
     '''
+    if not QC(u) or not QC(v): return RMISSD
     return (u**2 + v**2)**0.5
