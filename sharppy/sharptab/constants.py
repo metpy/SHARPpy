3eefbbc83902e16de51756a00d020daf55574ef2
Patrick Marsh <patrickmarshwx@gmail.com>
2011-12-26 09:13:30 -0600

    Added conversion and checking functions to the constants library.

--- sharppy/sharptab/constants.py
+++ sharppy/sharptab/constants.py
@@ -1,8 +1,46 @@
 ''' Frequently used constants '''
 
-__all__ = ["RMISSD", "ROCP", "ZEROCNK", "G"]
+consts = ["RMISSD", "ROCP", "ZEROCNK", "G"]
+funcs = ['MS2KTS', 'QC']
+__all__ = consts + funcs
 
+
+# Constants
 RMISSD = -9999.0            # Missing Flag
 ROCP = 0.28571426           # R divided by Cp
 ZEROCNK = 273.15            # Zero Celsius in Kelvins
 G = 9.80665                 # Earth's Gravity
+
+
+
+# Functions
+def MS2KTS(val):
+    '''
+    Convert meters per second to knots
+
+    Inputs
+    ------
+        val     (float)         Speed (m/s)
+
+    Returns
+    -------
+        Val converted to knots (float)
+    '''
+    return val * 1.94384449
+
+
+def QC(val):
+    '''
+    Check to see if given data is missing
+
+    Inputs
+    ------
+        val     (float)         Value to check
+
+    Returns
+    -------
+        1 if value is good
+        0 if value is bad
+    '''
+    if val < -998.0 or val > 2.0e5: return 0
+    return 1
