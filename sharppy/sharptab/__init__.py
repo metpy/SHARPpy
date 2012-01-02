824059b77e522c02505cbaa01add934fd93a8068
Patrick Marsh <patrickmarshwx@gmail.com>
2012-01-01 19:38:00 -0600

    Removed parcel.py from folder importing

--- sharppy/sharptab/__init__.py
+++ sharppy/sharptab/__init__.py
@@ -5,7 +5,6 @@ import profile
 import thermo
 import winds
 import params
-import parcel
 
 __all__ = ['constants', 'thermo', 'profile', 'vector', 'winds', 'interp',
-           'parcel', 'params']
+           'params']
