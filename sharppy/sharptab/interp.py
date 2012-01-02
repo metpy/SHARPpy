263e9e5f697170bfca2f0473e9c1b1870597499d
Patrick Marsh <patrickmarshwx@gmail.com>
2012-01-02 10:31:23 -0600

    Changed 'profile' to 'prof' and changed from fixed indices to dynamic inds

--- sharppy/sharptab/interp.py
+++ sharppy/sharptab/interp.py
@@ -10,71 +10,71 @@ __all__ = ['i_pres', 'i_hght', 'i_temp', 'i_dwpt', 'i_vec', 'i_vtmp',
            'interp_from_pres', 'interp_from_hght', 'agl', 'msl']
 
 
-def pres(h, profile):
+def pres(h, prof):
     '''
     Interpolates the given data to calculate a pressure at a given height
 
     Inputs
     ------
         h           (float)                 Height (m) of a level
-        profile     (profile object)        Profile object
+        prof        (profile object)        Profile object
 
     Returns
     -------
         Pressure (hPa) at the given height
     '''
-    return interp_from_hght(h, profile, 0)
+    return interp_from_hght(h, prof, prof.pind)
 
 
-def hght(p, profile):
+def hght(p, prof):
     '''
     Interpolates the given data to calculate a height at a given pressure
 
     Inputs
     ------
         p           (float)                 Pressure (hPa) of a level
-        profile     (profile object)        Profile object
+        prof        (profile object)        Profile object
 
     Returns
     -------
         Height (m) at the given pressure
     '''
-    return interp_from_pres(p, profile, 1)
+    return interp_from_pres(p, prof, prof.zind)
 
 
-def temp(p, profile):
+def temp(p, prof):
     '''
     Interpolates the given data to calculate a temperature at a given pressure
 
     Inputs
     ------
         p           (float)                 Pressure (hPa) of a level
-        profile     (profile object)        Profile object
+        prof        (profile object)        Profile object
 
     Returns
     -------
         Temperature (C) at the given pressure
     '''
-    return interp_from_pres(p, profile, 2)
+    return interp_from_pres(p, prof, prof.tind)
 
 
-def dwpt(p, profile):
+def dwpt(p, prof):
     '''
     Interpolates the given data to calculate a dew point at a given pressure
 
     Inputs
     ------
         p           (float)                 Pressure (hPa) of a level
-        profile     (profile object)        Profile object
+        prof        (profile object)        Profile object
 
     Returns
     -------
         Dew point (C) at the given pressure
     '''
-    return interp_from_pres(p, profile, 3)
+    return interp_from_pres(p, prof, prof.tdind)
 
 
-def vtmp(p, profile):
+def vtmp(p, prof):
     '''
     Interpolates the given data to calculate a virtual temperature at a
     given pressure
@@ -82,18 +82,18 @@ def vtmp(p, profile):
     Inputs
     ------
         p           (float)                 Pressure (hPa) of a level
-        profile     (profile object)        Profile object
+        prof        (profile object)        Profile object
 
     Returns
     -------
         Virtual temperature (C) at the given pressure
     '''
-    t = temp(p, profile)
-    td = dwpt(p, profile)
+    t = temp(p, prof)
+    td = dwpt(p, prof)
     return thermo.virtemp(p, t, td)
 
 
-def components(p, profile):
+def components(p, prof):
     '''
     Interpolates the given data to calculate the U and V components at a
     given pressure
@@ -101,14 +101,14 @@ def components(p, profile):
     Inputs
     ------
         p           (float)                 Pressure (hPa) of a level
-        profile     (profile object)        Profile object
+        prof        (profile object)        Profile object
 
     Returns
     -------
         U/V components at the given pressure
     '''
-    U = interp_from_pres(p, profile, 4)
-    V = interp_from_pres(p, profile, 5)
+    U = interp_from_pres(p, prof, prof.uind)
+    V = interp_from_pres(p, prof, prof.vind)
     return U, V
 
 
@@ -134,14 +134,14 @@ def vec(p, U, V):
 
 
 
-def interp_from_hght(h, profile, ind):
+def interp_from_hght(h, prof, ind):
     '''
     General interpolation routine for height coordinates
 
     Inputs
     ------
         h           (float)                 Height (m) of a level
-        profile     (profile object)        Profile object
+        prof        (profile object)        Profile object
         ind         (integer)               Index of variable to interpolate
 
     Returns
@@ -150,29 +150,29 @@ def interp_from_hght(h, profile, ind):
     '''
     tptr = -1
     bptr = 0
-    for i in range(profile.gNumLevels):
-        if not QC(profile.gSndg[i][ind]): continue
-        if profile.gSndg[i][1] < h:
+    for i in range(prof.gNumLevels):
+        if not QC(prof.gSndg[i][ind]): continue
+        if prof.gSndg[i][prof.zind] < h:
             bptr = i
-        elif profile.gSndg[i][1] == h:
-            return profile.gSndg[i][ind]
+        elif prof.gSndg[i][prof.zind] == h:
+            return prof.gSndg[i][ind]
         else:
             tptr = i
-            nm1 = h - profile.gSndg[bptr][1]
-            nm2 = profile.gSndg[tptr][1] - profile.gSndg[bptr][1]
-            val = profile.gSndg[bptr][ind] + ((nm1 / nm2) * \
-                (profile.gSndg[tptr][ind] - profile.gSndg[bptr][ind]))
+            nm1 = h - prof.gSndg[bptr][prof.zind]
+            nm2 = prof.gSndg[tptr][prof.zind] - prof.gSndg[bptr][prof.zind]
+            val = prof.gSndg[bptr][ind] + ((nm1 / nm2) * \
+                (prof.gSndg[tptr][ind] - prof.gSndg[bptr][ind]))
             return val
 
 
-def interp_from_pres(p, profile, ind):
+def interp_from_pres(p, prof, ind):
     '''
     General interpolation routine for pressure coordinates
 
     Inputs
     ------
         p           (float)                 Pressure (hPa) of a level
-        profile     (profile object)        Profile object
+        prof        (profile object)        Profile object
         ind         (integer)               Index of variable to interpolate
 
     Returns
@@ -181,51 +181,52 @@ def interp_from_pres(p, profile, ind):
     '''
     tptr = -1
     bptr = 0
-    for i in range(profile.gNumLevels):
-        if not QC(profile.gSndg[i][ind]): continue
-        if profile.gSndg[i][0] > p:
+    for i in range(prof.gNumLevels):
+        if not QC(prof.gSndg[i][ind]): continue
+        if prof.gSndg[i][prof.pind] > p:
             bptr = i
-        elif profile.gSndg[i][0] == p:
-            return profile.gSndg[i][ind]
+        elif prof.gSndg[i][prof.pind] == p:
+            return prof.gSndg[i][ind]
         else:
             tptr = i
-            nm1 = profile.gSndg[tptr][ind] - profile.gSndg[bptr][ind]
-            nm2 = math.log(profile.gSndg[bptr][0] / profile.gSndg[tptr][0])
-            nm3 = math.log(profile.gSndg[bptr][0] / p)
-            return profile.gSndg[bptr][ind] + ((nm3 / nm2) * nm1)
+            nm1 = prof.gSndg[tptr][ind] - prof.gSndg[bptr][ind]
+            nm2 = math.log(prof.gSndg[bptr][prof.pind] / \
+                           prof.gSndg[tptr][prof.pind])
+            nm3 = math.log(prof.gSndg[bptr][prof.pind] / p)
+            return prof.gSndg[bptr][ind] + ((nm3 / nm2) * nm1)
 
 
-def agl(h, profile):
+def agl(h, prof):
     '''
     Convert a height from mean sea-level (MSL) to above ground-level (AGL)
 
     Inputs
     ------
         h           (float)                 Height of a level
-        profile     (profile object)        Profile object
+        prof        (profile object)        Profile object
 
     Returns
     -------
         Converted height
     '''
     if not QC(h): return RMISSD
-    return h - profile.gSndg[profile.sfc][1]
+    return h - prof.gSndg[prof.sfc][prof.zind]
 
 
-def msl(h, profile):
+def msl(h, prof):
     '''
     Convert a height from above ground-level (AGL) to mean sea-level (MSL)
 
     Inputs
     ------
         h           (float)                 Height of a level
-        profile     (profile object)        Profile object
+        prof        (profile object)        Profile object
 
     Returns
     -------
         Converted height
     '''
     if not QC(h): return RMISSD
-    return h + profile.gSndg[profile.sfc][1]
+    return h + prof.gSndg[prof.sfc][prof.zind]
 
 
